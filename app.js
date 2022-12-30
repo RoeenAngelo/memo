70e6744da98b53fbfc1e5f7f79b2e803e89c9db1
Roeen <roeenacal@gmail.com>
2022-12-29 16:35:53 -0800

    Create elements using JS and addEeventListeners

--- app.js
+++ app.js
@@ -6,21 +6,36 @@ const taskContainer = document.querySelector('#task-container');
 
 
 addTask.addEventListener('click', () => {
+    
     let task = document.createElement('div');
-    task.classList.add('task');
+    task.classList.add('task'); // add a class
 
     let li = document.createElement('li');
-
     li.innerText = `${inputTask.value}`;
     task.appendChild(li);
 
     let checkBtn = document.createElement('button');
-    checkBtn.innerHTML = '<i class="fa-duotone fa-check"></i>'
+    checkBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    checkBtn.setAttribute('class', 'check-task'); // another way of adding a class
+    task.appendChild(checkBtn);
 
     let deleteBtn = document.createElement('button');
     deleteBtn.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+    deleteBtn.className = 'delete-task'; // another way of adding a class
+    task.appendChild(deleteBtn);
 
+    checkBtn.addEventListener('click', () => {
+        checkBtn.parentElement.style.textDecoration = 'line-through'
+    })
+
+    deleteBtn.addEventListener('click', (e) => {
+        e.target.parentElement.parentElement.remove();
+    })
+    
+    inputTask.value === '' ? alert('Please Enter a Task') : taskContainer.appendChild(task);
+    inputTask.value ='';
 })
+
 
 
 
